d12d6c933494eb437e387aaf700d35e1af57f5a9
Connor <61102529+Metater@users.noreply.github.com>
2024-12-01 13:53:18 -0500

    Initial commit

---
+++ Days/Day1/Part1.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode2024.Days.Day1;
+
+internal class Part1 : DayPart
+{
+    public override bool HasPrecedence => true;
+    //public override string InputFile => "Example.txt";
+    //public override bool ShouldRejectWhiteSpaceLines => false;
+
+    public override void Run(List<string> input)
+    {
+        List<int> left = [];
+        List<int> right = [];
+
+        foreach (var line in input)
+        {
+            string[] split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int l = int.Parse(split[0]);
+            int r = int.Parse(split[1]);
+
+            left.Add(l);
+            right.Add(r);
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            int l = TakeMin(left);
+            int r = TakeMin(right);
+
+            sum += GetDistance(l, r);
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static int TakeMin(List<int> list)
+    {
+        if (list.Count == 0)
+        {
+            throw new Exception();
+        }
+
+        int min = list.Min();
+        list.Remove(min);
+
+        return min;
+    }
+
+    private static int GetDistance(int left, int right)
+    {
+        return Math.Abs(right - left);
+    }
+}
---
+++ Days/Day1/Part2.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2024.Days.Day1;
+
+internal class Part2 : DayPart
+{
+    public override bool HasPrecedence => true;
+    //public override string InputFile => "Example.txt";
+    //public override bool ShouldRejectWhiteSpaceLines => false;
+
+    public override void Run(List<string> input)
+    {
+        List<int> left = [];
+        List<int> right = [];
+
+        foreach (var line in input)
+        {
+            string[] split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int l = int.Parse(split[0]);
+            int r = int.Parse(split[1]);
+
+            left.Add(l);
+            right.Add(r);
+        }
+
+        Dictionary<int, int> rightMultiplicity = [];
+
+        foreach (var r in right)
+        {
+            if (rightMultiplicity.ContainsKey(r))
+            {
+                rightMultiplicity[r]++;
+            }
+            else
+            {
+                rightMultiplicity[r] = 1;
+            }
+        }
+
+        int simularityScore = 0;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            int l = left[i];
+            simularityScore += l * rightMultiplicity.GetValueOrDefault(l, 0);
+        }
+
+        Console.WriteLine(simularityScore);
+    }
+}
