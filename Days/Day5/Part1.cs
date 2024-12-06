e1086e5dcc8deb0e77b6f5a78eba5f0c67382531
Connor <61102529+Metater@users.noreply.github.com>
2024-12-05 22:27:08 -0500

    Complete day 5

---
+++ Days/Day5/Part1.cs
@@ -0,0 +1,70 @@
+﻿namespace AdventOfCode2024.Days.Day5;
+
+internal class Part1 : DayPart
+{
+    public override bool HasPrecedence => true;
+    //public override string InputFile => "Example.txt";
+    public override bool ShouldRejectWhiteSpaceLines => false;
+
+    public override void Run(List<string> input)
+    {
+        bool foundEmptyLine = false;
+
+        List<(int X, int Y)> rules = [];
+        List<List<int>> requests = [];
+
+        foreach (var line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                foundEmptyLine = true;
+                continue;
+            }
+
+            if (!foundEmptyLine)
+            {
+                var split = line.Split('|');
+                rules.Add((int.Parse(split[0]), int.Parse(split[1])));
+            }
+            else
+            {
+                var split = line.Split(',', StringSplitOptions.TrimEntries);
+                var request = split.Select(int.Parse).ToList();
+                requests.Add(request);
+            }
+        }
+
+        int sum = 0;
+        foreach (var request in requests)
+        {
+            if (IsOrdered(request, rules))
+            {
+                sum += GetMiddleNumber(request);
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static int GetMiddleNumber(List<int> request)
+    {
+        return request[request.Count / 2];
+    }
+
+    private static bool IsOrdered(List<int> request, List<(int X, int Y)> rules)
+    {
+        var applicableRules = rules.Where(r => IsRuleApplicable(request, r)).ToList();
+
+        return applicableRules.All(r => IsRuleBeingFollowed(request, r));
+    }
+
+    private static bool IsRuleApplicable(List<int> request, (int X, int Y) rule)
+    {
+        return request.Contains(rule.X) && request.Contains(rule.Y);
+    }
+
+    private static bool IsRuleBeingFollowed(List<int> request, (int X, int Y) rule)
+    {
+        return request.IndexOf(rule.X) < request.IndexOf(rule.Y);
+    }
+}
